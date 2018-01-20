ecb8d1575b7a305d453ef4ac4140017d6f60b9d0
Ferenc Janky <fecjanky@gmail.com>
2018-01-20 17:54:49 +0100

    temp commit

--- scheduler_lib/include/job_graph.h
+++ scheduler_lib/include/job_graph.h
@@ -1,9 +1,33 @@
 #pragma once
 
 #include <initializer_list>
+#include <type_traits>
+#include <utility>
 #include <vector>
 
 namespace job_sheduler {
+
+namespace detail {
+template <typename EdgeT, typename NodeF>
+struct node_type {
+public:
+    using from_type = decltype(from_type_func());
+    using to_type = decltype(to_type_func());
+    using type = std::common_type_t<from_type, to_type>;
+
+private:
+    auto from_type_func()
+    {
+        auto && [ from, to ] = std::declval<NodeF>()(std::declval<EdgeT>());
+        return from;
+    }
+    auto to_type_func()
+    {
+        auto && [ from, to ] = std::declval<NodeF>()(std::declval<EdgeT>());
+        return to;
+    }
+};
+} // namespace detail
 
 template <typename EdgeT, typename NodeF>
 class graph : private NodeF {
@@ -11,11 +35,16 @@ public:
     graph(NodeF node_func, std::initializer_list<EdgeT> edges)
         : NodeF(node_func)
     {
-        auto&&[from, to] = node_function()(*edges.begin());
     }
 
 private:
     const NodeF& node_function() const { return *this };
-    NodeF& node_function(){ return *this };
+    NodeF& node_function() { return *this; };
 };
+
+template <typename EdgeT, typename NodeF>
+auto make_graph(NodeF f, std::initializer_list<EdgeT> edges)
+{
+    return graph<EdgeT, NodeF>(std::move(f), std::move(edges));
 }
+} // namespace job_sheduler
--- test/src/test_graph.cpp
+++ test/src/test_graph.cpp
@@ -1,1 +1,12 @@
 #include <catch.hpp>
+
+#include <utility>
+
+#include <job_graph.h>
+
+TEST_CASE("test graph construction", "[graph]")
+{
+
+    auto graph = job_sheduler::make_graph([](auto&& e) {return e; }, { std::make_pair("a","b"),std::make_pair("b", "c") });
+    int a = 0;
+}
