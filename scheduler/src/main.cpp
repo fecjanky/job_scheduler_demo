11fcd30d5d594ac1160349238b56fe642f35f412
Ferenc Janky <fecjanky@gmail.com>
2018-01-21 21:51:02 +0100

    changed lib type to header only

--- scheduler/src/main.cpp
+++ scheduler/src/main.cpp
@@ -1,6 +1,12 @@
+#include <job_graph.h>
+#include <string>
+#include <utility>
 
+using namespace std::literals;
 
 int main()
 {
+    job_sheduler::make_graph({ std::make_pair("a"s, "b"s),
+        std::make_pair("b"s, "c"s), std::make_pair("b"s, "d"s) });
     return 0;
 }
