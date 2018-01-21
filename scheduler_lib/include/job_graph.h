fabb9455712ce2d889fc08ae52f16a80a5200a78
Ferenc Janky <fecjanky@gmail.com>
2018-01-21 19:28:32 +0100

    wip

--- scheduler_lib/include/job_graph.h
+++ scheduler_lib/include/job_graph.h
@@ -55,52 +55,86 @@ template <typename EdgeT, typename VertexF>
 class graph {
 public:
     using vertex_type = detail::vertex_type_t<EdgeT, VertexF>;
+    using vertices_t = std::vector<vertex_type>;
+    using vertex_const_iterator = typename vertices_t::const_iterator;
+    struct vertex_ref;
+    using vertex_ref_pointer = vertex_ref*;
+    using vertex_ref_const_pointer = const vertex_ref*;
+    using edges_t = std::vector<vertex_ref_pointer>;
+    struct vertex_ref {
+        explicit vertex_ref(vertex_const_iterator it)
+            : vertex(it)
+        {
+        }
+        vertex_const_iterator vertex;
+        edges_t out;
+        edges_t in;
+    };
+    using graph_t = std::vector<vertex_ref>;
+    using graph_iterator = typename graph_t::iterator;
+    using graph_const_iterator = typename graph_t::const_iterator;
+    using view_t = std::vector<graph_iterator>;
+    using view_iterator = typename view_t::iterator;
+    using view_const_iterator = typename view_t::const_iterator;
 
     graph(VertexF vertex_func, const std::initializer_list<EdgeT>& edges);
 
     template <typename Iterator>
     graph(VertexF vertex_func, Iterator begin, Iterator end);
 
+    graph(graph&&) = default;
+    graph& operator=(graph&&) = default;
+
+    graph(const graph&);
+    graph& operator=(const graph&) = delete;
+    ~graph() = default;
+
+    view_const_iterator find(const vertex_type& key) const;
+
+    view_const_iterator end() const;
+
     size_t num_vertices() const noexcept;
     size_t num_edges() const noexcept;
 
+    std::pair<std::vector<vertex_type>, graph> next_schedule() const;
+
 private:
     class impl;
-    using vertices_t = std::vector<vertex_type>;
-    using vertex_const_iterator_t = typename vertices_t::const_iterator;
-    using edges_t = std::vector<vertex_const_iterator_t>;
-    static constexpr size_t VERTEX = 0;
-    static constexpr size_t EDGES_OUT = 1;
-    static constexpr size_t EDGES_IN = 2;
-    static constexpr size_t IMPL_EDGES_OUT = 0;
-    static constexpr size_t IMPL_EDGES_IN = 1;
+
+    void init_from_impl();
+    void restore_heap();
+
+    void check_entry();
+    static auto graph_compare()
+    {
+        return [](const auto& lhs, const auto& rhs) {
+            return lhs->in.size() > rhs->in.size();
+        };
+    }
 
     /*************************************/
-
-    std::vector<std::tuple<vertex_const_iterator_t, edges_t, edges_t>> m_graph;
     std::shared_ptr<const impl> m_impl;
-
+    graph_t m_graph;
+    view_t m_view;
     /*************************************/
+    /*Resource sharing of the Node object*/
     struct impl : private VertexF {
         using vertices_t = std::vector<vertex_type>;
         using vertex_const_iterator_t = typename vertices_t::const_iterator;
-        using edges_t = std::vector<vertex_const_iterator_t>;
-        using io_edges_t = std::vector<std::tuple<edges_t, edges_t>>;
+        using edges_to = std::vector<std::vector<vertex_const_iterator_t>>;
 
         template <typename Iterator>
         impl(VertexF vertex_func, Iterator begin, Iterator end);
 
         size_t num_vertices() const noexcept;
         const vertices_t& vertices() const noexcept;
-        const io_edges_t& edges() const noexcept;
+        const edges_to& edges() const noexcept;
 
         template <typename Iterator>
         void init_vertices(Iterator begin, Iterator end);
 
         template <typename Iterator>
         void init_edges(Iterator begin, Iterator end);
-
-        void checkDAG();
 
         vertex_const_iterator_t find_vertex(const vertex_type& vertex);
 
@@ -109,7 +143,7 @@ private:
 
         /*********************/
         vertices_t m_vertices;
-        io_edges_t m_edges;
+        edges_to m_edges;
     };
 
 }; // namespace job_sheduler
@@ -126,20 +160,7 @@ inline graph<EdgeT, VertexF>::graph(
     VertexF vertex_func, Iterator begin, Iterator end)
     : m_impl(std::make_shared<const impl>(std::move(vertex_func), begin, end))
 {
-    m_graph.reserve(m_impl->num_vertices());
-    for (auto it = m_impl->vertices().begin(); it != m_impl->vertices().end();
-         ++it) {
-        m_graph.emplace_back(it,
-            std::get<IMPL_EDGES_OUT>(
-                m_impl->edges()[std::distance(m_impl->vertices().begin(), it)]),
-            std::get<IMPL_EDGES_IN>(m_impl->edges()[std::distance(
-                m_impl->vertices().begin(), it)]));
-    }
-    std::make_heap(
-        m_graph.begin(), m_graph.end(), [](const auto& lhs, const auto& rhs) {
-            return std::get<EDGES_IN>(lhs).size()
-                > std::get<EDGES_IN>(rhs).size();
-        });
+    init_from_impl();
 }
 
 template <typename EdgeT, typename VertexF>
@@ -147,6 +168,38 @@ inline graph<EdgeT, VertexF>::graph(
     VertexF vertex_func, const std::initializer_list<EdgeT>& edges)
     : graph(std::move(vertex_func), edges.begin(), edges.end())
 {
+}
+
+template <typename EdgeT, typename VertexF>
+inline graph<EdgeT, VertexF>::graph(const graph& other)
+    : m_impl(other.m_impl)
+{
+    m_view.reserve(other.m_view.size());
+    m_graph.reserve(other.m_view.size());
+    // Filter nodes from view
+    std::transform(other.m_view.begin(), other.m_view.end(),
+        std::back_inserter(m_graph),
+        [](const auto& v) { return vertex_ref(v->vertex); });
+    // Filter edges based on filtered nodes
+    for (auto& g : m_graph) {
+        const auto& edges
+            = m_impl->edges()[std::distance(m_impl->vertices(), g.vertex)];
+    }
+    // build representation
+}
+
+template <typename EdgeT, typename VertexF>
+inline auto graph<EdgeT, VertexF>::find(const vertex_type& key) const
+    -> view_const_iterator
+{
+    return std::find_if(m_view.begin(), m_view.end(),
+        [&key](const auto& elem) { return *(elem->vertex) == key; });
+}
+
+template <typename EdgeT, typename VertexF>
+inline auto graph<EdgeT, VertexF>::end() const -> view_const_iterator
+{
+    return m_view.end();
 }
 
 template <typename EdgeT, typename VertexF>
@@ -159,9 +212,64 @@ template <typename EdgeT, typename VertexF>
 inline size_t graph<EdgeT, VertexF>::num_edges() const noexcept
 {
     return std::accumulate(m_graph.begin(), m_graph.end(), size_t(0),
-        [](auto accu, const auto& v) {
-            return accu + std::get<EDGES_OUT>(v).size();
-        });
+        [](auto accu, const auto& v) { return accu + v.out.size(); });
+}
+
+template <typename EdgeT, typename VertexF>
+inline auto graph<EdgeT, VertexF>::next_schedule() const
+    -> std::pair<std::vector<vertex_type>, graph>
+{
+
+    return std::pair<std::vector<vertex_type>, graph>();
+}
+
+template <typename EdgeT, typename VertexF>
+inline void graph<EdgeT, VertexF>::init_from_impl()
+{
+    m_graph.clear();
+    m_view.clear();
+    const auto& impl_vertices = m_impl->vertices();
+    const auto& impl_edges = m_impl->edges();
+    m_graph.reserve(impl_vertices.size());
+    /*init vertices references*/
+    for (auto v_it = impl_vertices.begin(); v_it != impl_vertices.end();
+         ++v_it) {
+        m_graph.emplace_back(v_it);
+    }
+    /*init edges in graph*/
+    for (auto edges = impl_edges.begin(); edges != impl_edges.end(); ++edges) {
+        auto& from_vertex = m_graph[std::distance(impl_edges.begin(), edges)];
+        from_vertex.out.reserve(edges->size());
+        for (auto edge_it = edges->begin(); edge_it != edges->end();
+             ++edge_it) {
+            auto& to_vertex
+                = m_graph[std::distance(impl_vertices.begin(), *edge_it)];
+            from_vertex.out.push_back(std::addressof(to_vertex));
+            to_vertex.in.push_back(std::addressof(from_vertex));
+        }
+    }
+    /*init view*/
+    m_view.reserve(m_graph.size());
+    for (auto view_it = m_graph.begin(); view_it != m_graph.end(); ++view_it) {
+        m_view.push_back(view_it);
+    }
+
+    restore_heap();
+    check_entry();
+}
+
+template <typename EdgeT, typename VertexF>
+inline void graph<EdgeT, VertexF>::restore_heap()
+{
+    std::make_heap(m_view.begin(), m_view.end(), graph_compare());
+}
+
+template <typename EdgeT, typename VertexF>
+inline void graph<EdgeT, VertexF>::check_entry()
+{
+    if (!m_view.empty() && !m_view.front()->in.empty()) {
+        throw std::runtime_error("no entry point in graph");
+    }
 }
 
 template <typename EdgeT, typename VertexF>
@@ -203,12 +311,7 @@ inline void graph<EdgeT, VertexF>::impl::init_edges(
         const auto & [ from, to ] = vertex_function()(edge);
         const auto from_it = find_vertex(from);
         const auto to_it = find_vertex(to);
-        std::get<IMPL_EDGES_OUT>(
-            m_edges[std::distance(m_vertices.cbegin(), from_it)])
-            .push_back(to_it);
-        std::get<IMPL_EDGES_IN>(
-            m_edges[std::distance(m_vertices.cbegin(), to_it)])
-            .push_back(from_it);
+        m_edges[std::distance(m_vertices.cbegin(), from_it)].push_back(to_it);
     });
 }
 
@@ -227,7 +330,7 @@ inline auto graph<EdgeT, VertexF>::impl::vertices() const noexcept
 
 template <typename EdgeT, typename VertexF>
 inline auto graph<EdgeT, VertexF>::impl::edges() const noexcept
-    -> const io_edges_t&
+    -> const edges_to&
 {
     return m_edges;
 }
--- test/src/test_graph.cpp
+++ test/src/test_graph.cpp
@@ -4,13 +4,36 @@
 
 #include <job_graph.h>
 
-TEST_CASE("test graph construction", "[graph]")
-{
+using namespace job_sheduler;
 
-    auto graph = job_sheduler::make_graph([](auto&& e) { return e; },
+auto create_test_graph()
+{
+    return make_graph([](auto&& e) { return e; },
         { std::make_pair(std::string("a"), std::string("b")),
             std::make_pair(std::string("b"), std::string("c")),
             std::make_pair(std::string("b"), std::string("d")) });
+}
+
+TEST_CASE("test graph construction", "[graph]")
+{
+
+    auto graph = create_test_graph();
     REQUIRE(graph.num_vertices() == 4);
     REQUIRE(graph.num_edges() == 3);
+}
+
+TEST_CASE("test find node in graph", "[graph]")
+{
+    auto graph = create_test_graph();
+    REQUIRE(graph.find("d") != graph.end());
+    REQUIRE(graph.find("s") == graph.end());
+}
+
+TEST_CASE(
+    "job graph with no entry point (i.e. in degree is 0) can't be created",
+    "[graph]")
+{
+    REQUIRE_THROWS(make_graph([](auto&& e) { return e; },
+        { std::make_pair(std::string("a"), std::string("b")),
+            std::make_pair(std::string("b"), std::string("a")) }));
 }
