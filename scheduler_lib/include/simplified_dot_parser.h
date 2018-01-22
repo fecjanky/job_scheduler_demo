e814dcd4b3d591a826fceca139996f828ae6d419
Ferenc Janky <fecjanky@gmail.com>
2018-01-22 19:25:46 +0100

    added simplified dot parser

---
+++ scheduler_lib/include/simplified_dot_parser.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <istream>
+#include <regex>
+#include <string>
+#include <utility>
+
+namespace job_sheduler {
+namespace utils {
+
+/// \brief parsing simplified dot file with format
+/// \verbatim
+///  digraph G{
+///    "a" -> "b";
+///    "b" -> "c";
+///  }
+/// \endverbatim
+/// simplifications on file format: digraph opening statment followed
+/// by edge listings one per line (use singele quote iside string literal
+/// , terminated by closing bracket on a new line
+inline auto parse_simplified_dot(std::istream& dot_text)
+{
+
+    static const std::regex digraph_start(R"#(\s*digraph\s+[^{]\s*\{\s*)#");
+    static const std::regex digraph_edge(
+        R"#(\s*"([^"]+)"\s*->\s*"([^"]+)"\s*;\s*)#");
+    static const std::regex digraph_end(R"#(\s*\}\s*)#");
+    static const std::regex digraph_empty(R"#(\s*)#");
+
+    enum class parse_state {
+        START,
+        EDGES,
+        END,
+    } state
+        = parse_state::START;
+
+    std::vector<std::pair<std::string, std::string>> res;
+    size_t i = 0;
+    std::string line;
+    while (dot_text && std::getline(dot_text, line)) {
+        ++i;
+        const auto throw_error = [i, &line]() {
+            throw std::runtime_error(
+                "dotfile error on line " + std::to_string(i) + ':' + line);
+        };
+
+        if (std::regex_match(line, digraph_empty))
+            continue;
+        switch (state) {
+        case parse_state::START: {
+            if (std::regex_match(line, digraph_start))
+                state = parse_state::EDGES;
+            else
+                throw_error();
+        } break;
+        case parse_state::EDGES: {
+            if (std::smatch match; std::regex_search(line, match, digraph_edge))
+                res.emplace_back(match[1], match[2]);
+            else if (std::regex_match(line, digraph_end))
+                state = parse_state::END;
+            else
+                throw_error();
+        } break;
+        case parse_state::END:
+            break;
+        default:
+            throw std::logic_error(
+                "error in simplified dot parser state machine");
+        };
+    }
+    if (state != parse_state::END) {
+        throw std::runtime_error("error in simplified dot format");
+    }
+    return res;
+} // namespace utils
+
+} // namespace utils
+} // namespace job_sheduler
---
+++ test/src/test_dot_parser.cpp
@@ -0,0 +1,78 @@
+#include <catch.hpp>
+
+#include <simplified_dot_parser.h>
+
+#include <sstream>
+#include <string>
+
+using namespace std::string_literals;
+using namespace job_sheduler::utils;
+
+TEST_CASE("dot parser on a simple graph", "[dot parser]")
+{
+    constexpr auto simple_dot = R"#(digraph G {
+    "a" -> "b";
+    "b" -> "c";
+    })#";
+    std::istringstream iss(simple_dot);
+    auto res = parse_simplified_dot(iss);
+    REQUIRE(res.size() == 2);
+    auto & [ from_0, to_0 ] = res[0];
+    auto & [ from_1, to_1 ] = res[1];
+    REQUIRE(from_0 == "a");
+    REQUIRE(to_0 == "b");
+    REQUIRE(from_1 == "b");
+    REQUIRE(to_1 == "c");
+}
+
+TEST_CASE("dot parser on the reference graph", "[dot parser]")
+{
+    constexpr auto simple_dot = R"#(
+
+digraph G {
+    "a" -> "g";
+
+    "b" -> "c";
+    "b" -> "d";
+
+
+    "g" -> "h";
+    "g" -> "i";
+    "c" -> "e";
+    "d" -> "e";
+    "h" -> "j";
+    "i" -> "j";
+    "e" -> "f";
+    "j" -> "f";
+
+    }
+
+
+
+)#";
+    std::istringstream iss(simple_dot);
+    auto res = parse_simplified_dot(iss);
+    REQUIRE(res.size() == 11);
+    auto & [ from_0, to_0 ] = res[0];
+    auto & [ from_1, to_1 ] = res[1];
+    auto & [ from_2, to_2 ] = res[2];
+    auto & [ from_3, to_3 ] = res[3];
+    auto & [ from_4, to_4 ] = res[4];
+    auto & [ from_5, to_5 ] = res[5];
+    auto & [ from_6, to_6 ] = res[6];
+    auto & [ from_7, to_7 ] = res[7];
+    auto & [ from_8, to_8 ] = res[8];
+    auto & [ from_9, to_9 ] = res[9];
+    auto & [ from_10, to_10 ] = res[10];
+    REQUIRE((from_0 == "a" && to_0 == "g"));
+    REQUIRE((from_1 == "b" && to_1 == "c"));
+    REQUIRE((from_2 == "b" && to_2 == "d"));
+    REQUIRE((from_3 == "g" && to_3 == "h"));
+    REQUIRE((from_4 == "g" && to_4 == "i"));
+    REQUIRE((from_5 == "c" && to_5 == "e"));
+    REQUIRE((from_6 == "d" && to_6 == "e"));
+    REQUIRE((from_7 == "h" && to_7 == "j"));
+    REQUIRE((from_8 == "i" && to_8 == "j"));
+    REQUIRE((from_9 == "e" && to_9 == "f"));
+    REQUIRE((from_10 == "j" && to_10 == "f"));
+}
